7aeee39ef1523fd34e42e4b48f7b801deb784f0d
Gaspar <rigos.gasparis@gmail.com>
2024-03-25 19:49:22 +0200

    added booking table and filtering acceptance method to room class

--- app/src/main/java/com/example/mogbnb/Room.java
+++ app/src/main/java/com/example/mogbnb/Room.java
@@ -1,4 +1,7 @@
 package com.example.mogbnb;
+import java.time.temporal.ChronoUnit;
+import java.time.LocalDate;
+
 
 public class Room {
     private String roomName;
@@ -9,6 +12,8 @@ public class Room {
     private int noOfReviews;
     private float pricePerDay;
     private String roomImage; //Path to url of the image
+    private boolean[] bookingTable;
+    private static LocalDate currentDate;
 
     public Room(String roomName, int noOfPersons, int availableDays, String area, double stars, int noOfReviews, String roomImage, float pricePerDay) {
         this.roomName = roomName;
@@ -19,6 +24,41 @@ public class Room {
         this.noOfReviews = noOfReviews;
         this.roomImage = roomImage;
         this.pricePerDay = pricePerDay;
+        bookingTable = new boolean[availableDays];
+        //initiate all days as not booked in the booking table
+        for(int i = 0; i<availableDays; i++){
+            bookingTable[i] = false;
+        }
+        if(currentDate == null){
+            currentDate = LocalDate.now();
+        }
+    }
+
+
+    /**
+     * Method used to determine if a filter is accepted by a room
+     * @param filter the filter given to a room
+     * @return true if the filter matches the room, false if not.
+     */
+    public boolean filterAccepted(Filter filter){
+        LocalDate checkOutDate = filter.getCheckOut();
+        LocalDate checkInDate = filter.getCheckIn();
+
+        long bookingDaysTotal = ChronoUnit.DAYS.between(checkInDate, checkOutDate);
+
+        int indexOfCheckInDate = (int) ChronoUnit.DAYS.between(Room.currentDate, checkInDate);
+
+        //check if filtered days are not booked
+        for (int i = 0; i<bookingDaysTotal; i++){
+            if(bookingTable[indexOfCheckInDate + i]){
+                return false;
+            }
+        }
+        //filtered days are all available from this point onward
+
+        return (filter.getArea() == null || filter.getArea().equalsIgnoreCase(this.area))&&(filter.getPrice()==0 || filter.getPrice()>=this.pricePerDay)&&(filter.getStars()<=this.stars)
+                &&(filter.getNoOfPersons()==0 || filter.getNoOfPersons()==this.noOfPersons)
+                &&(bookingDaysTotal<= availableDays)&&((int) ChronoUnit.DAYS.between(Room.currentDate, checkOutDate)<=availableDays);
     }
 
     /**
