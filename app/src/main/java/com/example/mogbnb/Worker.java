05e4d48d11c73dd60f00f13ea55c4f8c0392904f
anestis_th <p3210273@aueb.gr>
2024-03-28 20:36:06 +0200

    Worker abstract first idea implemntation

---
+++ app/src/main/java/com/example/mogbnb/Worker.java
@@ -0,0 +1,24 @@
+package com.example.mogbnb;
+
+import java.util.List;
+
+public class Worker {
+    private int id ;
+    private List<Room> roomData;
+
+    public Worker(){
+        this.id++;
+        this.roomData=roomData;
+    }
+
+    //Method to handle adding a room
+    public void addRoom(Room room){
+        //Add a room
+    }
+
+    //Method to handle booking a room
+    //h booking tha periexei ta booking instances
+    public void bookRoom(Booking booking){
+        //Process the booking process
+    }
+}
