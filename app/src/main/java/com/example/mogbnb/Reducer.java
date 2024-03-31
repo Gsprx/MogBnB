fb15e3bd17737421d77f1f9a19d434c087ccc0d8
Gaspar <rigos.gasparis@gmail.com>
2024-03-31 22:24:57 +0300

    small change to Reducer class to add mapID case functionality

--- app/src/main/java/com/example/mogbnb/Reducer.java
+++ app/src/main/java/com/example/mogbnb/Reducer.java
@@ -1,13 +1,12 @@
 package com.example.mogbnb;
 
 import com.example.misc.Config;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
+import java.util.ArrayList;
 
 public class Reducer extends Thread {
     /* Socket for receiving requests */
@@ -19,6 +18,8 @@ public class Reducer extends Thread {
     public void run() {
         // create the server socket
         try {
+            //output list from the combined inputs of the workers
+            ArrayList<Room> outputRoomList;
             server = new ServerSocket(Config.WORKER_REDUCER_PORT);
 
             while (true) {
@@ -33,13 +34,13 @@ public class Reducer extends Thread {
                     Socket replyToMaster = new Socket("localhost", Config.REDUCER_MASTER_PORT);
                     ObjectOutputStream out = new ObjectOutputStream(replyToMaster.getOutputStream());
 
-                    out.writeInt(mapID);
-                    out.writeObject(in.readObject());
-                    out.flush();
+                    if (mapID == MasterFunction.FIND_ROOM_BY_NAME.getEncoded()) {
+                        out.writeInt(mapID);
+                        out.writeObject(in.readObject());
+                        out.flush();
+                    }
 
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (ClassNotFoundException e) {
+                } catch (IOException | ClassNotFoundException e) {
                     throw new RuntimeException(e);
                 }
             }
