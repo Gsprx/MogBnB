d0f9670f8a80fd444c4f835fa3bfa6c1597f0b02
Gaspar <rigos.gasparis@gmail.com>
2024-04-14 18:21:31 +0300

    updated error message for tenant's case of booking when another tenants books one date requested at the same time fixed gree nfn

--- app/src/main/java/com/example/mogbnb/MasterThread.java
+++ app/src/main/java/com/example/mogbnb/MasterThread.java
@@ -343,7 +343,7 @@ public class MasterThread extends Thread {
             int result = reducer_in.readInt();
 
             if (result == 1) out.writeObject("Rating updated successfully.");
-            else out.writeObject("Booking was unsuccessful, days requested were already booked!");
+            else out.writeObject("An error occurred while rating this room.");
             out.flush();
 
             reducer_in.close();
@@ -388,7 +388,7 @@ public class MasterThread extends Thread {
             int result = reducer_in.readInt();
 
             if (result == 1) out.writeObject("Booking successful.");
-            else out.writeObject("An error occured.");
+            else out.writeObject("Booking was unsuccessful, days requested were already booked!");
             out.flush();
 
             reducer_in.close();
