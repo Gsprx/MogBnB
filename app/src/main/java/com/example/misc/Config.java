01b36b03cffb3e01623155a45c32634207a2a0e7
Phlorion <gigadroll44thebest@gmail.com>
2024-04-15 20:55:11 +0300

    merge fixes

--- app/src/main/java/com/example/misc/Config.java
+++ app/src/main/java/com/example/misc/Config.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.example.misc;
 
 public class Config {
@@ -8,22 +7,7 @@ public class Config {
     public static int WORKER_REDUCER_PORT = 6000;
     public static int REDUCER_MASTER_PORT = 7000; //used by reducers to send refined results of workers to master
 
-    public static String MASTER_IP = "172.16.1.52";
-    public static String[] WORKER_IP = {"172.16.1.54", "172.16.1.54", "172.16.1.52"};
-    public static String REDUCER_IP = "172.16.1.54";
+    public static String MASTER_IP = "localhost";
+    public static String[] WORKER_IP = {"localhost", "localhost", "localhost"};
+    public static String REDUCER_IP = "localhost";
 }
-=======
-package com.example.misc;
-
-public class Config {
-    public static int NUM_OF_WORKERS = 3;
-    public static int USER_MASTER_PORT = 4444;
-    public static int INIT_WORKER_PORT = 5000;
-    public static int WORKER_REDUCER_PORT = 6000;
-    public static int REDUCER_MASTER_PORT = 7000; //used by reducers to send refined results of workers to master
-
-    public static String MASTER_IP = "172.16.1.52";
-    public static String[] WORKER_IP = {"172.16.1.54", "172.16.1.54", "172.16.1.52"};
-    public static String REDUCER_IP = "172.16.1.54";
-}
->>>>>>> 518239e2de8ff90ed6596a65454bf1f4171cb852
