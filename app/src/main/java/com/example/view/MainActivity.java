c880310069349273807eb3ad9988a3bb403f4b73
Phlorion <gigadroll44thebest@gmail.com>
2024-05-08 15:43:58 +0300

    working navbar and fragments

--- app/src/main/java/com/example/view/MainActivity.java
+++ app/src/main/java/com/example/view/MainActivity.java
@@ -1,16 +1,57 @@
 package com.example.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.mogbnb.R;
+import com.example.view.fragments.BookingsFragment;
+import com.example.view.fragments.ProfileFragment;
+import com.example.view.fragments.SearachFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView navbar;
+    SearachFragment search;
+    BookingsFragment bookings;
+    ProfileFragment profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create search fragment
+        search = new SearachFragment();
+        // create bookings fragment
+        bookings = new BookingsFragment();
+        // create profile fragment
+        profile = new ProfileFragment();
+        // // when the main appears set the frame layout to the search fragment
+        replaceFragment(search);
+
+        // get navbar
+        navbar = findViewById(R.id.main_bottomNavView);
+        // manage navbar fragments
+        navbar.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_search) replaceFragment(search);
+            else if (item.getItemId() == R.id.nav_bookings) replaceFragment(bookings);
+            else replaceFragment(profile);
+            return true;
+        });
+    }
+
+    /**
+     * Αλλαγή του fragment
+     * @param fragment Το fragment στο οποίο θέλουμε να αλλάξουμε
+     */
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
