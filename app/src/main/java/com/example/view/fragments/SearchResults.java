ed6b15c3acaa5f1ec3a97428d3a44edf7b1f49e0
Gaspar <rigos.gasparis@gmail.com>
2024-05-09 21:08:42 +0300

    added ui elements for search room page and its a general recycler view item layout for rooms

---
+++ app/src/main/java/com/example/view/fragments/SearchResults.java
@@ -0,0 +1,24 @@
+package com.example.view.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SearchResults extends Fragment {
+
+    public SearchResults() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return null;
+    }
+}
