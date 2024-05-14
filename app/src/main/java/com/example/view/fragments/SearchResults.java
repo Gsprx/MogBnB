1006f3be743ad7723f0d011369960e90d7fc3717
Gaspar <rigos.gasparis@gmail.com>
2024-05-14 18:18:07 +0300

    completed search results ui code and recycler view adapter code

--- app/src/main/java/com/example/view/fragments/SearchResults.java
+++ app/src/main/java/com/example/view/fragments/SearchResults.java
@@ -2,11 +2,21 @@ package com.example.view.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.mogbnb.R;
+import com.example.mogbnb.Room;
+import com.example.view.recyclerViewAdapters.SelectRoomRVAdapter;
+
+import java.util.ArrayList;
 
 
 public class SearchResults extends Fragment {
@@ -19,6 +29,36 @@ public class SearchResults extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return null;
+        View view = inflater.inflate(R.layout.fragment_search_results, container, false);
+        return view;
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+        //get rooms passed by the filter fragment
+        ArrayList<Room> rooms = getArguments()!=null ? (ArrayList<Room>) getArguments().getSerializable("rooms") : new ArrayList<>();
+
+        //get RV reference
+        RecyclerView recyclerView = view.findViewById(R.id.rvSearchResults);
+        //create RV adapter
+        SelectRoomRVAdapter adapter = new SelectRoomRVAdapter(this, rooms);
+
+        //set adapter's click listener
+        adapter.setClickListener((view1, position) -> {
+            //set code to run when a room is clicked
+            showRoomDetails(rooms.get(position));
+        });
+
+        //bind adapter to RV
+        recyclerView.setAdapter(adapter);
+        //set layout for RV
+        //TODO: Check if this code works (green fn implementation).
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
+    }
+
+    //method is called when a room is clicked in the list
+    private void showRoomDetails(Room room) {
+        //TODO: Start room details fragment
+
     }
 }
---
+++ app/src/main/java/com/example/view/recyclerViewAdapters/SelectRoomRVAdapter.java
@@ -0,0 +1,121 @@
+package com.example.view.recyclerViewAdapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mogbnb.R;
+import com.example.mogbnb.Room;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class SelectRoomRVAdapter extends RecyclerView.Adapter<SelectRoomRVAdapter.SelectRoomViewHolder> {
+    private ArrayList<Room> rooms;
+    private Context context;
+    private ItemClickListener clickListener;
+
+
+    public SelectRoomRVAdapter(Fragment fragment, ArrayList<Room> rooms){
+        this.rooms = rooms;
+        if (fragment.getContext()!=null){
+            this.context = fragment.getContext();
+        }
+        else{
+            throw new IllegalStateException("Search Results Fragment is not attached to context.");
+        }
+    }
+    @NonNull
+    @Override
+    //Creates the look for the rows in the UI recycler view
+    public SelectRoomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_view_room_item, parent, false);
+        return new SelectRoomViewHolder(view);
+    }
+
+    @Override
+    //Assign(bind) the data to use for each row
+    //changes the data on the recycler view based on the position of the recycler
+    public void onBindViewHolder(@NonNull SelectRoomViewHolder holder, int position) {
+        //set each holder's member fields to match the data of the Room data found on the position given in the list of filtered rooms
+        holder.roomName.setText(rooms.get(position).getRoomName());
+
+        //we use a decimal formatter to clean up the result of the doubles in the room class such as price (if needed) and rating.
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        String formattedPrice = df.format(rooms.get(position).getPricePerDay());
+        holder.roomPrice.setText(formattedPrice);
+
+        String formattedRating = df.format(rooms.get(position).getStars());
+        holder.roomRating.setText(formattedRating);
+
+        holder.roomNumOfReviews.setText(rooms.get(position).getNoOfReviews());
+
+        //TODO: set image later
+
+
+
+    }
+
+
+    //catch click events with a click listener that we set
+    public void setClickListener(ItemClickListener listener){
+        this.clickListener = listener;
+    }
+
+    @Override
+    public int getItemCount() {
+        return rooms.size();
+    }
+
+
+    //method of this interface must be implemented by parent activity
+    //parent activity is responsible for what to do when a room is clicked
+    public interface ItemClickListener{
+        void onItemClick(View view,int position);
+    }
+
+
+
+    //this class holds the View objects in the recycler view room item layout file
+    //additionally handles the click listening
+    public class SelectRoomViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+
+        TextView roomName;
+        TextView roomPrice;
+        TextView roomRating;
+        TextView roomNumOfReviews;
+        ImageView roomImage;
+
+        public SelectRoomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+            roomName = itemView.findViewById(R.id.textRoomItemName);
+            roomPrice = itemView.findViewById(R.id.textRoomItemPrice);
+            roomRating = itemView.findViewById(R.id.textRoomItemRating);
+            roomNumOfReviews = itemView.findViewById(R.id.textRoomItemNumReviews);
+            roomImage = itemView.findViewById(R.id.imageRoomItem);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (clickListener != null) {
+                clickListener.onItemClick(view, getAdapterPosition());
+            }
+        }
+
+    }
+
+}
+
+
