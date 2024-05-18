6358d38ca834ff24725e2229bc06b049244b77fb
Gaspar <rigos.gasparis@gmail.com>
2024-05-18 15:02:30 +0300

    updated project sdk values

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.mogbnb"
-        minSdk = 31
+        minSdk = 26
         targetSdk = 31
         versionCode = 1
         versionName = "1.0"
