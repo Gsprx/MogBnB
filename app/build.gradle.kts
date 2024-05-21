2290c9d290db0e95d400e6a5775b30bab6f06fff
Gaspar <rigos.gasparis@gmail.com>
2024-05-21 17:15:18 +0300

    upgraded target sdk to 34 api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "com.example.mogbnb"
         minSdk = 26
-        targetSdk = 26
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
